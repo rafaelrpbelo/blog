8fe033e185d3281724a9ba02b967b0fbd16e69c1
Rafael Belo <rafaelrpbelo@gmail.com>
2014-04-14 15:37:26 -0300

    Internacionalização

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,26 +4,26 @@ feature 'Post' do
 	scenario 'main' do
 		visit '/posts'
 
-		expect(page).to have_content 'Posts'
+		expect(page).to have_content t('title.post.index')
 	end
 
 	scenario 'new post button' do
 		visit '/posts'
 
-		expect(page).to have_content 'Criar Post'
+		expect(page).to have_content title('post.index')
 	end
 
 	scenario 'create post' do
 		visit '/posts'
 
-		click_link 'Criar Post'
-		expect(page).to have_content 'Novo Post'
+		click_link link('post.new')
+		expect(page).to have_content t('title.post.new')
 
-		fill_in 'Title', with: 'Post 1'
-		fill_in 'Body', with: 'Body 1'
-		click_button 'Criar Post'
+		fill_in label('title'), with: 'Post 1'
+		fill_in label('body'), with: 'Body 1'
+		click_button submit('post')
 
-		expect(page).to have_content 'Post criado com sucesso'
+		expect(page).to have_content t('flash.post.create.notice')
 		expect(page).to have_content 'Post 1'
 		expect(page).to	have_content 'Body 1'
 	end
