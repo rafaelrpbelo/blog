70b229f4a662a055f12e9e60df31dfafcf326529
Rafael Belo <rafaelrpbelo@gmail.com>
2014-05-02 00:08:36 -0300

    Helper create errors_explanation
    Create errors_explanation that receive a resource and show in the form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def errors_explanation(resource)
+    if resource.errors.any?
+      messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+      html = <<-HTML
+      <div id=''>
+        <h3>#{ I18n.t 'form.error.message' }</h3>
+        <ul>#{ messages }</ul>
+      </div>
+      HTML
+
+      html.html_safe
+    end
+  end
 end
